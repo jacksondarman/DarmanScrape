5daef0d71df79b53986ecf478508ccde1ca0d567
Jackson Darman <jackson@darman.dev>
2024-09-11 07:06:38 -0400

    got my db out of source control hehe

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,32 +1,30 @@
-package tests
+package parser
 
 import (
 	"reflect"
 	"testing"
-
-	"github.com/jacksondarman/lekscrape/internal/parser"
 )
 
 // Correctly parses civilizations from well-formed text input
 func TestParseCivilizationsWellFormedInput(t *testing.T) {
 	text := "Rome - Julius Caesar\nAbility: Roman Legions - Can build roads\nImprovement: Aqueduct - Provides fresh water\nBuilding: Colosseum - Increases happiness\nUnit: Legion - Stronger than swordsman\nGreat Person: Augustus - Increases culture\nBias: Coastal"
 
-	expected := []parser.Civilization{
+	expected := []Civilization{
 		{
 			Name:   "Rome",
 			Leader: "Julius Caesar",
 			Bias:   "Coastal",
-			UAbility: []parser.Ability{{
+			UAbility: []Ability{{
 				Name:   "Roman Legions",
 				Effect: "Can build roads",
 			}},
-			UImprovements: []parser.Improvement{{Name: "Aqueduct", Effect: "Provides fresh water"}},
-			UBuildings:    []parser.Building{{Name: "Colosseum", Effect: "Increases happiness"}},
-			UUnits:        []parser.Unit{{Name: "Legion", Effect: "Stronger than swordsman"}},
-			UGreatPerson:  []parser.GreatPerson{{Name: "Augustus", Effect: "Increases culture"}},
+			UImprovements: []Improvement{{Name: "Aqueduct", Effect: "Provides fresh water"}},
+			UBuildings:    []Building{{Name: "Colosseum", Effect: "Increases happiness"}},
+			UUnits:        []Unit{{Name: "Legion", Effect: "Stronger than swordsman"}},
+			UGreatPerson:  []GreatPerson{{Name: "Augustus", Effect: "Increases culture"}},
 		},
 	}
-	result := parser.ParseCivilizations(text)
+	result := ParseCivilizations(text)
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Expected %v, but got %v", expected, result)
 	}
@@ -36,20 +34,20 @@ func TestParseCivilizationsWellFormedInput(t *testing.T) {
 func TestParseCivilizationsMissingBlocks(t *testing.T) {
 	text := "Rome - Julius Caesar\nAbility: Roman Legions - Can build roads\nBias: Coastal"
 
-	expected := []parser.Civilization{
+	expected := []Civilization{
 		{
 			Name:          "Rome",
 			Leader:        "Julius Caesar",
 			Bias:          "Coastal",
-			UAbility:      []parser.Ability{{Name: "Roman Legions", Effect: "Can build roads"}},
-			UImprovements: []parser.Improvement{},
-			UBuildings:    []parser.Building{},
-			UUnits:        []parser.Unit{},
-			UGreatPerson:  []parser.GreatPerson{},
+			UAbility:      []Ability{{Name: "Roman Legions", Effect: "Can build roads"}},
+			UImprovements: []Improvement{},
+			UBuildings:    []Building{},
+			UUnits:        []Unit{},
+			UGreatPerson:  []GreatPerson{},
 		},
 	}
 
-	result := parser.ParseCivilizations(text)
+	result := ParseCivilizations(text)
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Expected %v, but got %v", expected, result)
 	}
